87663bf7e7da55ceb25f2844568119280c1da6bf
WSantanH <wayllahnsantanadossantos@gmail.com>
2023-10-22 21:38:31 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+const button = document.getElementById("button");
+
+button.addEventListener("click", () => {
+    button.style.margin = "0 auto";
+    button.style.background = "#D5D5D5";
+    button.style.color = "#FFF";
+    button.style.padding = "10px";
+});
